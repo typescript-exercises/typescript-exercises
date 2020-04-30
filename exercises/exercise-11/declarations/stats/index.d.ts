d200a2e52d725dd33ea8a32ee1f717b5c589c2dc
Marat Dulin <dulin@arrival.com>
2020-04-30 12:17:06 +0200

    Exercise 11: Solution.

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,13 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type Comparator<I> = (a: I, b: I) => number;
+    type StatIndexFunction = <I>(input: I[], comparator: Comparator<I>) => number;
+    type StatElementFunction = <I>(input: I[], comparator: Comparator<I>) => I;
+
+    export const getMaxIndex: StatIndexFunction;
+    export const getMinIndex: StatIndexFunction;
+    export const getMedianIndex: StatIndexFunction;
+    export const getMaxElement: StatElementFunction;
+    export const getMinElement: StatElementFunction;
+    export const getMedianElement: StatElementFunction;
+    export const getAverageValue: <I, O>(items: I[], getValue: (item: I) => O) => O;
 }
