5fe4879945f2d211125398bdf6ff25200fab2b99
hanafi <hanafi.abderrahmen@appsintegra.com>
2020-05-10 20:42:07 +0400

    reset refactor to original

--- exercises/exercise-07/index.ts
+++ exercises/exercise-07/index.ts
@@ -1,4 +1,4 @@
-import chalk from "chalk"
+import chalk from 'chalk';
 
 /*
 
@@ -28,82 +28,75 @@ Run:
 */
 
 interface User {
-  type: "user"
-  name: string
-  age: number
-  occupation: string
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
 }
 
 interface Admin {
-  type: "admin"
-  name: string
-  age: number
-  role: string
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
 }
 
-type PowerUser = unknown
+type PowerUser = unknown;
 
-type Person = User | Admin | PowerUser
+type Person = User | Admin | PowerUser;
 
 const persons: Person[] = [
-  {
-    type: "user",
-    name: "Max Mustermann",
-    age: 25,
-    occupation: "Chimney sweep",
-  },
-  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
-  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
-  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
-  {
-    type: "powerUser",
-    name: "Nikki Stone",
-    age: 45,
-    role: "Moderator",
-    occupation: "Cat groomer",
-  },
-]
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+    {
+        type: 'powerUser',
+        name: 'Nikki Stone',
+        age: 45,
+        role: 'Moderator',
+        occupation: 'Cat groomer'
+    }
+];
 
 function isAdmin(person: Person): person is Admin {
-  return person.type === "admin"
+    return person.type === 'admin';
 }
 
 function isUser(person: Person): person is User {
-  return person.type === "user"
+    return person.type === 'user';
 }
 
 function isPowerUser(person: Person): person is PowerUser {
-  return person.type === "powerUser"
+    return person.type === 'powerUser';
 }
 
 function logPerson(person: Person) {
-  let additionalInformation: string = ""
-  if (isAdmin(person)) {
-    additionalInformation = person.role
-  }
-  if (isUser(person)) {
-    additionalInformation = person.occupation
-  }
-  if (isPowerUser(person)) {
-    additionalInformation = `${person.role}, ${person.occupation}`
-  }
-  console.log(
-    `${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`
-  )
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    if (isPowerUser(person)) {
+        additionalInformation = `${person.role}, ${person.occupation}`;
+    }
+    console.log(`${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
 }
 
-console.log(chalk.yellow("Admins:"))
-persons.filter(isAdmin).forEach(logPerson)
+console.log(chalk.yellow('Admins:'));
+persons.filter(isAdmin).forEach(logPerson);
 
-console.log()
+console.log();
 
-console.log(chalk.yellow("Users:"))
-persons.filter(isUser).forEach(logPerson)
+console.log(chalk.yellow('Users:'));
+persons.filter(isUser).forEach(logPerson);
 
-console.log()
+console.log();
 
-console.log(chalk.yellow("Power users:"))
-persons.filter(isPowerUser).forEach(logPerson)
+console.log(chalk.yellow('Power users:'));
+persons.filter(isPowerUser).forEach(logPerson);
 
 // In case if you are stuck:
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
