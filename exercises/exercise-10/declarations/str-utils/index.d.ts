62fbc968c9bb6870507ecd04c6a9f9cbab8a025f
huahaitao <huahaitao@baidu.com>
2020-05-16 19:32:44 +0800

    compelete exercise-10 && 11

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,7 @@
 declare module 'str-utils' {
-    // export const ...
-    // export function ...
+    export function strToLower(str: string): string;
+    export function strToUpper(str: string): string;
+    export function strReverse(str: string): string;
+    export function strInvertCase(str: string): string;
+    export function strRandomize(str: string): string;
 }
--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,9 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export function getMaxIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMaxElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): T | null;
+    export function getMinIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMinElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): T | null;
+    export function getAverageValue<T>(input: Array<T>, comparator: (a: T, b: T) => number): number | null;
+    export function getMedianIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMedianElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): T | null;
 }
