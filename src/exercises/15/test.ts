d9a15626ca94762db7ccb77a8ac406c942eec545
Pavlos Vinieratos <pvinis@gmail.com>
2020-08-14 14:02:53 +0200

    Update test.ts

--- src/exercises/15/test.ts
+++ src/exercises/15/test.ts
@@ -1,14 +1,14 @@
 import {typeAssert, IsTypeEqual} from 'type-assertions';
-import {ObjectManupulator} from './index';
+import {ObjectManipulator} from './index';
 
-const test1 = new ObjectManupulator({})
+const test1 = new ObjectManipulator({})
     .set('x', 123)
     .set('y', 'hello')
     .getObject();
 
 typeAssert<IsTypeEqual<typeof test1, {x: number; y: string}>>();
 
-const test2 = new ObjectManupulator({})
+const test2 = new ObjectManipulator({})
     .set('x', 123)
     .set('y', 'hello')
     .set('z', true)
@@ -18,7 +18,7 @@ const test2 = new ObjectManupulator({})
 
 typeAssert<IsTypeEqual<typeof test2, {x: number}>>();
 
-const test3 = new ObjectManupulator({})
+const test3 = new ObjectManipulator({})
     .set('x', 123)
     .set('y', 'hello')
     .delete('y')
@@ -26,7 +26,7 @@ const test3 = new ObjectManupulator({})
 
 typeAssert<IsTypeEqual<typeof test3, number>>();
 
-const test4 = new ObjectManupulator({x: true, y: 'hello'})
+const test4 = new ObjectManipulator({x: true, y: 'hello'})
     .delete('y')
     .get('x');
 
