a0075ced53551b8b8a3b2a751def83aab3150770
DaliaAsh <daliayousef1998@yahoo.com>
2020-11-08 14:25:56 +0200

    Function Overloading

--- src/exercises/6/index.ts
+++ src/exercises/6/index.ts
@@ -52,17 +52,19 @@ export const persons: Person[] = [
     { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
 ];
 
+const getObjectKeys = <T>(obj: T) => Object.keys(obj) as (keyof T)[];
 export function logPerson(person: Person) {
     console.log(
         ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
     );
 }
-
-export function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
+export function filterPersons(persons: Person[], personType: 'admin', criteria: Partial<Admin>): Admin[];
+export function filterPersons(persons: Person[], personType: 'user', criteria: Partial<User>): User[];
+export function filterPersons(persons: Person[], personType: string, criteria: Partial<Person>): Person[] {
     return persons
         .filter((person) => person.type === personType)
         .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+            let criteriaKeys = getObjectKeys(criteria);
             return criteriaKeys.every((fieldName) => {
                 return person[fieldName] === criteria[fieldName];
             });
