0b255324f97549baacff7724c818a89daa2b8d74
DaliaAsh <daliayousef1998@yahoo.com>
2020-11-10 10:54:57 +0200

    declartion file generics

--- src/exercises/12/declarations/stats/index.d.ts
+++ src/exercises/12/declarations/stats/index.d.ts
@@ -1,3 +1,16 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type CompareFunction<T> = (value1: T, value2: T) => number;
+    type GetIndexValue = <T>(input: T[], comparator: CompareFunction<T>) => number;
+    export const getMaxIndex: GetIndexValue;
+    export const getMinIndex: GetIndexValue;
+    export const getMedianIndex: GetIndexValue;
+
+    type GetElementValue = <T>(input: T[], comparator: CompareFunction<T>) => null | T;
+    export const getMaxElement: GetElementValue;
+    export const getMinElement: GetElementValue;
+    export const getMedianElement: GetElementValue;
+    type GetValueFromObject<T> = (item: T) => number;
+    type GetValue = <T>(value1: T[], GetValueFromObject: GetValueFromObject<T>) => number | null;
+
+    export const getAverageValue: GetValue;
 }
