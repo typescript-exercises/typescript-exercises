62c4886a2300b073b2f97a26e392875aefeffec2
imadaqel <emadaqel92@gmail.com>
2021-03-09 21:04:01 +0200

    first ex

--- src/exercises/1/index.ts
+++ src/exercises/1/index.ts
@@ -76,9 +76,13 @@ Exercise:
 
 */
 
-export type User = unknown;
+export interface User {
+    name: string,
+    age: number,
+    occupation: string
+};
 
-export const users: unknown[] = [
+export const users: User[] = [
     {
         name: 'Max Mustermann',
         age: 25,
@@ -91,7 +95,7 @@ export const users: unknown[] = [
     }
 ];
 
-export function logPerson(user: unknown) {
+export function logPerson(user: User) {
     console.log(` - ${user.name}, ${user.age}`);
 }
 
