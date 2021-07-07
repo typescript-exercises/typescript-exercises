4c239acc2af4403231786cb91de7291f8a99e9d7
Hana D <h.murshed@foothillsolutions.com>
2021-07-07 15:12:57 +0300

    mu comment

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root')//ok
 );
 
 // If you want your app to work offline and load faster, you can change
