203b1eba57d8b0ff9fdcdcea628d35d89722d5c8
Atheer5 <atheersabaneh67@gmail.com>
2021-07-07 14:20:32 +0300

    solution of exercise00

---
+++ src/exercises/1/Exercise00.solution.ts
@@ -0,0 +1,27 @@
+
+export interface User {
+    name:string;
+    age:number;
+    occupation:string;
+
+};
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
