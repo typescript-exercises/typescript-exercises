5d298a4df8a26b99ab609b70144fcbedaad57925
mqashoo77 <mohammadqashoo77@gmail.com>
2022-10-02 21:20:30 +0300

    exercise one is done

--- src/exercises/1/index.ts
+++ src/exercises/1/index.ts
@@ -76,27 +76,31 @@ Exercise:
 
 */
 
-export type User = unknown;
-
-export const users: unknown[] = [
+export interface User {
+    name: string;
+    age: number;
+    occupation: string ;
+  }
+  
+  export const users: User[] = [
     {
         name: 'Max Mustermann',
         age: 25,
-        occupation: 'Chimney sweep'
+        occupation: '23'
     },
     {
         name: 'Kate Müller',
         age: 23,
         occupation: 'Astronaut'
     }
-];
-
-export function logPerson(user: unknown) {
-    console.log(` - ${user.name}, ${user.age}`);
-}
-
-console.log('Users:');
-users.forEach(logPerson);
+  ];
+  
+  export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.occupation}`);
+  }
+  
+  console.log('Users:');
+  users.forEach(logPerson);
 
 
 /* In case if you are stuck:
